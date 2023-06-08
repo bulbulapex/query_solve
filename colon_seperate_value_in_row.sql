bb96eca449f55f61c5af900fe13c7be26674553c
bulbulapex <135580438+bulbulapex@users.noreply.github.com>
2023-06-08 18:02:58 +0600

    1:2:3 eita ke alada alada row kore abar ekshathe dekhano

--- colon_seperate_value_in_row.sql
+++ colon_seperate_value_in_row.sql
@@ -5,8 +5,17 @@ with emps (shuttle_item) as
     connect by level <= regexp_count(shuttle_item, ':') + 1;
     
     
-    with emps (CAT_LICENCE) as
-      (SELECT CAT_LICENCE FROM CONT_PROFILE WHERE CONT_ID = 17)
-    select regexp_substr(CAT_LICENCE, '[^:]+', 1, level) one_item
-    from emps
-    connect by level <= regexp_count(CAT_LICENCE, ':') + 1;
+with emps (CAT_LICENCE) as
+(SELECT CAT_LICENCE FROM CONT_PROFILE WHERE CONT_ID = 17)
+select regexp_substr(CAT_LICENCE, '[^:]+', 1, level) one_item
+from emps
+connect by level <= regexp_count(CAT_LICENCE, ':') + 1;
+
+
+/*1:2:3 ey value gulake , row akare vag kore data return kore then listagg diye abar 1 ta row te value dekhano holo*/
+SELECT LISTAGG(CL_NAME_ENG,', ') INTO V_CAT_LICENCE_NAME
+      FROM CAT_LICENCE
+      WHERE CL_ID IN (
+          select * from table(apex_string.split(V_CAT_LICENCE,':'))
+
+      )
